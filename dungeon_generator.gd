305f30149a7f844c1292fd339d35b4e71a715854
MuriloGurski <91623642+MuriloGurski@users.noreply.github.com>
2026-06-08 23:06:46 -0300

    Create dungeon generator
    Created dungeon generator scene

---
+++ dungeon_generator.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var directions = [
+	Vector2i(1, 0),
+	Vector2i(-1, 0),
+	Vector2i(0, 1),
+	Vector2i(0, -1)
+]
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
