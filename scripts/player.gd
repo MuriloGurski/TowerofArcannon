9bcf36478b0dac6f5b5248e2d3bf2260f04ed772
PedroS2962 <pedrohenriquemachados641@gmail.com>
2026-06-18 16:36:35 -0300

    Dodge animation play (without animation frames)

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var dodge_speed := 500.0
+var dodge_duration := 0.25
 
 var last_direction: Vector2 = Vector2.RIGHT
 var is_attacking: bool = false
+var is_dodging: bool = false
+var dodge_direction: Vector2 = Vector2.ZERO
+var dodge_timer := 0.0
+
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -13,8 +19,10 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		attack()
+	if Input.is_action_just_pressed("dodge") and not is_dodging:
+		dodge()
 		
-	#Parar de se mover ao atacard
+	#Parar de se mover ao atacar
 	if is_attacking:
 		velocity = Vector2.ZERO
 		return
@@ -28,8 +36,6 @@ func _physics_process(_delta: float) -> void:
 	#----------------------------------------#
 	
 func movements() -> void: 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
 	
 	if direction != Vector2.ZERO:
@@ -38,10 +44,13 @@ func movements() -> void:
 	else:
 		velocity = Vector2.ZERO
 	
+	
 
-#Define quando será tocada a animação Idle e a Run	
+#Define quando será tocada a animação Idle, Run, Attack e Dodge	
 func animation_type() -> void:
 	if is_attacking:
+		return
+	if is_dodging:
 		return
 	if velocity != Vector2.ZERO:
 		play_animation("run", last_direction)
@@ -58,6 +67,29 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down")
+
+
+#----------------------------------------#
+#	     		 DODGE			 		 #
+#----------------------------------------#
+
+func dodge()-> void:
+	dodge_direction = Vector2.RIGHT
+		
+	is_dodging = true
+	dodge_timer = dodge_duration
+	play_animation("dodge", last_direction)
+	
+func handle_dodge(delta)-> void:
+	velocity = dodge_direction * dodge_speed
+	
+	dodge_timer -= delta
+	
+	if dodge_timer <= 0:
+		is_dodging = false
+		velocity = Vector2.ZERO
+		
+
 		
 	#----------------------------------------#
 	#	     		 ATACAR			 		 #
@@ -70,3 +102,6 @@ func attack()-> void:
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if is_attacking:
 		is_attacking = false
+	if is_dodging:
+		is_dodging = false
+	
