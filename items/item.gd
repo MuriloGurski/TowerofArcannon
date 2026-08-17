7ae6bcb2548c2ce283873c251817baf15c07e6ae
Murilo Gurski <91623642+MuriloGurski@users.noreply.github.com>
2026-08-17 16:34:11 -0300

    Reorganized folders and created Item resource

---
+++ items/item.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Item
+
+@export var name : String = "Unknown Item"
+@export var texture : Texture2D
+@export var is_stackable : bool = false
+@export var max_stack : int = 99
