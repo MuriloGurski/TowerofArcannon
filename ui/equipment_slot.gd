b6ec33caaaee2f5967cd7ebcb52d8bd33841ba74
Murilo Gurski <91623642+MuriloGurski@users.noreply.github.com>
2026-08-17 15:51:48 -0300

    Early implementation of equipment

--- ui/equipment_slot.gd
+++ ui/equipment_slot.gd
@@ -1,11 +1,20 @@
 extends Panel
 
-
+var grid_position : Vector2i
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_grid_position(coords: Vector2i) -> void:
+	grid_position = coords
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			_on_slot_clicked()
+			
+func _on_slot_clicked():
+	print("Clicked visual slot at grid coordinate: ", grid_position)
--- ui/inventory.gd
+++ ui/inventory.gd
@@ -1,10 +1,20 @@
 extends Control
 
 @onready var inventory_slots : GridContainer = $"Inventory Container/Inventory Slots"
+@onready var equipment_box : HBoxContainer = $"Inventory Container/Equipment Box"
 
 var inventory_map : Dictionary = {}
+var equipment_map : Dictionary = {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_ready_inventory()
+	_ready_equipment()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _ready_inventory():
 	
 	var cols : int = inventory_slots.columns
 	var children = inventory_slots.get_children()
@@ -23,7 +33,34 @@ func _ready() -> void:
 			slot_node.set_grid_position(coords)
 		
 	print("Inventory map initialized: ", inventory_map.size(), " slots mapped.")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready_equipment():
+	
+	var equipment_left = equipment_box.get_child(0)
+	var equipment_right = equipment_box.get_child(2)
+	
+	var left_children = equipment_left.get_children()
+	var right_children = equipment_right.get_children()
+	
+	for i in range(left_children.size()):
+		
+		var slot_node = left_children[i]
+		
+		var y = i
+		var coords = Vector2i(0,y)
+		equipment_map[coords] = null
+		
+		if slot_node.has_method("set_grid_position"):
+			slot_node.set_grid_position(coords)
+	
+	for i in range(right_children.size()):
+		
+		var slot_node = right_children[i]
+		
+		var y = i
+		var coords = Vector2i(1,y)
+		equipment_map[coords] = null
+		
+		if slot_node.has_method("set_grid_position"):
+			slot_node.set_grid_position(coords)
+	
+	print("Equipment map initialized: ", equipment_map.size(), " slots mapped.")
