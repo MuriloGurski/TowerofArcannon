55d28e4a9d23d8b26cbb655a53190a0451b9165a
Murilo Gurski <91623642+MuriloGurski@users.noreply.github.com>
2026-08-21 14:15:25 -0300

    Finished implementation of _add_item and _move_item

--- ui/inventory/inventory.gd
+++ ui/inventory/inventory.gd
@@ -6,6 +6,7 @@ extends Control
 var hold_coord = null
 var inventory_map : Dictionary = {}
 var equipment_map : Dictionary = {}
+var inventory_slot_nodes : Dictionary = {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_ready_inventory()
@@ -29,6 +30,7 @@ func _ready_inventory():
 		var coords = Vector2i(x,y)
 		
 		inventory_map[coords] = null
+		inventory_slot_nodes[coords] = slot_node
 		
 		if slot_node.has_method("set_grid_position"):
 			slot_node.set_grid_position(coords)
@@ -51,6 +53,7 @@ func _ready_equipment():
 		var y = i
 		var coords = Vector2i(0,y)
 		equipment_map[coords] = null
+		
 		
 		if slot_node.has_method("set_grid_position"):
 			slot_node.set_grid_position(coords)
@@ -80,41 +83,66 @@ func _find_free_inventory_slot():
 			pass
 			
 	print("Inventory Full")
-func _on_inventory_slot_clicked(coords : Vector2i, event_button : int):
-	print("inventory call")
+func _on_inventory_slot_clicked(coords: Vector2i, event_button: int):
+	print("Inventory call")
+	
 	if event_button == MOUSE_BUTTON_LEFT:
-		print("Left click on: ", coords)
+		
 		if hold_coord == null:
-			hold_coord = coords
-			print("holding: ", coords)
-		elif inventory_map[hold_coord] != null:
-			print("moving")
-			_move_item(inventory_map[hold_coord], hold_coord, coords)
+			
+			if inventory_map[coords] != null:
+				hold_coord = coords
+				print("Holding: ", coords)
+				
+		else:
+			
+			if hold_coord == coords:
+				hold_coord = null
+				print("Cancelled holding item")
+				return
+				
+			_move_item(hold_coord, coords)
+			
 	elif event_button == MOUSE_BUTTON_RIGHT:
-		print("Right click on: ", coords)
+		
 		if inventory_map[coords] == null:
 			var test_ring = load("res://items/ring.tres") as Item
 			_add_item(test_ring, coords)
-		else:
-			pass
+
+
 func _on_equipment_slot_clicked(coords : Vector2i, event_button : int):
 	if event_button == MOUSE_BUTTON_LEFT:
 		print("Left click on: ", coords)
 	elif event_button == MOUSE_BUTTON_RIGHT:
 		print("Right click on: ", coords)
-func _add_item(item : Item, coords : Vector2i):
+func _add_item(item: Item, coords: Vector2i):
+	
 	if inventory_map[coords] == null:
+		
 		inventory_map[coords] = item
-		"added item"
+		var slot_node = inventory_slot_nodes[coords]
+		
+		if slot_node.has_method("set_item"):
+			slot_node.set_item(item)
+			
+		print("Added item")
 	else:
 		print("Slot Occupied")
-func _move_item(item : Item, original_coords : Vector2i, target_coords : Vector2i):
-	if inventory_map[target_coords] == null:
-		inventory_map[original_coords] = null
-		inventory_map[target_coords] = item
-		hold_coord = null
-		print("moved")
-	else:
-		hold_coord = null
-		print("failed to move")
+		
+func _move_item(original_coords: Vector2i, target_coords: Vector2i):
+	
+	var item = inventory_map[original_coords]
+	if item == null:
 		return
+		
+	if inventory_map[target_coords] != null:
+		print("Target occupied")
+		return
+		
+	inventory_map[original_coords] = null
+	inventory_map[target_coords] = item
+	
+	inventory_slot_nodes[original_coords].set_item(null)
+	inventory_slot_nodes[target_coords].set_item(item)
+	
+	hold_coord = null
--- ui/inventory/inventory_slot.gd
+++ ui/inventory/inventory_slot.gd
@@ -3,6 +3,7 @@ extends Panel
 signal inventory_slot_clicked(coords: Vector2i, event_button : int)
 
 var grid_position : Vector2i
+@onready var item_sprite: TextureRect = $"Item Sprite"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,3 +17,10 @@ func set_grid_position(coords: Vector2i) -> void:
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		inventory_slot_clicked.emit(grid_position, event.button_index)
+	
+func set_item(item : Item):
+	if item == null:
+		item_sprite.texture = null
+		return
+		
+	item_sprite.texture = item.texture
